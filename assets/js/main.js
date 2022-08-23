a657b100404da96f216a01e69bd7cf7178131865
Arjun Mohanesh <31795399+Esdark@users.noreply.github.com>
2022-08-23 11:46:54 +0530

    Update

---
+++ assets/js/main.js
@@ -0,0 +1,205 @@
+/* =====================================================
+Template Name   : Webex
+Description     : Web Design And Development Agency HTML5 Templat
+Author          : Themesland
+Version         : 1.0
+=======================================================*/
+
+
+(function ($) {
+    "use strict";
+
+
+    // data-background    
+    $(document).on('ready', function () {
+        $("[data-background]").each(function () {
+            $(this).css("background-image", "url(" + $(this).attr("data-background") + ")");
+        });
+    });
+
+
+    // wow init
+    new WOW().init();
+
+
+    // hero slider
+    $('.hero-slider').owlCarousel({
+        loop: true,
+        nav: true,
+        dots: false,
+        margin: 0,
+        autoplay: true,
+        autoplayHoverPause: true,
+        autoplayTimeout: 5000,
+        items: 1,
+        navText: [
+            "<i class='fal fa-long-arrow-left'></i>",
+            "<i class='fal fa-long-arrow-right'></i>"
+        ],
+    });
+
+    $('.hero-slider').on('change.owl.carousel', function (event) {
+        new WOW().init();
+    });
+
+
+    // partner-slider
+    $('.partner-slider').owlCarousel({
+        loop: true,
+        margin: 60,
+        nav: false,
+        dots: false,
+        autoplay: true,
+        responsive: {
+            0: {
+                items: 2
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 6
+            }
+        }
+    });
+
+
+    // testimonial-slider
+    $('.testimonial-slider').owlCarousel({
+        loop: true,
+        margin: 30,
+        nav: false,
+        dots: true,
+        autoplay: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 4
+            }
+        }
+    });
+
+
+
+    // preloader
+    $(window).on('load', function () {
+        $(".preloader").fadeOut("slow");
+    });
+
+
+    // fun fact counter
+    $('.counter').countTo();
+    $('.counter-box').appear(function () {
+        $('.counter').countTo();
+    }, {
+        accY: -100
+    });
+
+
+    // progress bar
+    if($('.progress-line').length){
+        $('.progress-line').appear(function(){
+            var el = $(this);
+            var percent = el.data('width');
+            $(el).css('width',percent+'%');
+        },{accY: 0});
+    };
+
+
+    // magnific popup init
+    $(".popup-gallery").magnificPopup({
+        delegate: '.popup-img',
+        type: 'image',
+        gallery: {
+            enabled: true
+        },
+    });
+
+    $(".popup-youtube, .popup-vimeo, .popup-gmaps").magnificPopup({
+        type: "iframe",
+        mainClass: "mfp-fade",
+        removalDelay: 160,
+        preloader: false,
+        fixedContentPos: false
+    });
+
+
+
+    // scroll to top
+    $(window).scroll(function () {
+        if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+            $("#scroll-top").addClass('active');
+        } else {
+            $("#scroll-top").removeClass('active');
+        }
+    });
+
+    $("#scroll-top").click(function () {
+        $("html, body").animate({ scrollTop: 0 }, 1500);
+        return false;
+    });
+
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
+            $('.navbar').addClass("fixed-top");
+        } else {
+            $('.navbar').removeClass("fixed-top");
+        }
+    });
+
+
+    // countdown
+    if ($('#countdown').length) {
+        $('#countdown').countdown('2028/01/30', function (event) {
+            $(this).html(event.strftime('' + '<div class="row">' + '<div class="col countdown-single">' + '<h2 class="mb-0">%-D</h2>' + '<h5 class="mb-0">Day%!d</h5>' + '</div>' + '<div class="col countdown-single">' + '<h2 class="mb-0">%H</h2>' + '<h5 class="mb-0">Hours</h5>' + '</div>' + '<div class="col countdown-single">' + '<h2 class="mb-0">%M</h2>' + '<h5 class="mb-0">Minutes</h5>' + '</div>' + '<div class="col countdown-single">' + '<h2 class="mb-0">%S</h2>' + '<h5 class="mb-0">Seconds</h5>' + '</div>' + '</div>'));
+        });
+    }
+
+
+    // project filter
+    $(window).on('load', function () {
+        if ($(".filter-box").children().length > 0) {
+            $(".filter-box").isotope({
+                itemSelector: '.filter-item',
+                masonry: {
+                    columnWidth: 1
+                },
+            });
+
+            $('.filter-btns').on('click', 'li', function () {
+                var filterValue = $(this).attr('data-filter');
+                $(".filter-box").isotope({ filter: filterValue });
+            });
+
+            $(".filter-btns li").each(function () {
+                $(this).on("click", function () {
+                    $(this).siblings("li.active").removeClass("active");
+                    $(this).addClass("active");
+                });
+            });
+        }
+    });
+
+
+    // copywrite date
+    let date = new Date().getFullYear();
+    $("#date").html(date);
+
+
+})(jQuery);
+
+
+
+
+
+
+
+
+
+
